c5634bd15c2db82bfe82bedc3548083b9fcdced8
François Kooman <fkooman@tuxed.net>
2014-10-21 10:08:38 +0200

    initial commit

---
+++ src/fkooman/Rest/Plugin/Basic/BasicAuthentication.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+* Copyright 2014 François Kooman <fkooman@tuxed.net>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+namespace fkooman\Rest\Plugin\Basic;
+
+use fkooman\Http\Request;
+use fkooman\Rest\ServicePluginInterface;
+use fkooman\Http\Exception\UnauthorizedException;
+
+class BasicAuthentication implements ServicePluginInterface
+{
+    /** @var string */
+    private $basicAuthUser;
+
+    /** @var string */
+    private $basicAuthPass;
+
+    /** @var string */
+    private $basicAuthRealm;
+
+    public function __construct($basicAuthUser, $basicAuthPass, $basicAuthRealm = "Protected Resource")
+    {
+        $this->basicAuthUser = $basicAuthUser;
+        $this->basicAuthPass = $basicAuthPass;
+        $this->basicAuthRealm = $basicAuthRealm;
+    }
+
+    public function execute(Request $request)
+    {
+        $requestBasicAuthUser = $request->getBasicAuthUser();
+        $requestBasicAuthPass = $request->getBasicAuthPass();
+
+        if ($this->basicAuthUser !== $requestBasicAuthUser) {
+            throw new UnauthorizedException("invalid credentials", 'Basic', array('realm' => $this->basicAuthRealm));
+        }
+
+        if (!password_verify($requestBasicAuthPass, $this->basicAuthPass)) {
+            throw new UnauthorizedException("invalid credentials", 'Basic', array('realm' => $this->basicAuthRealm));
+        }
+
+        return new UserInfo($this->basicAuthUser);
+    }
+}
---
+++ tests/fkooman/Rest/Plugin/Basic/BasicAuthenticationTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+* Copyright 2014 François Kooman <fkooman@tuxed.net>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+namespace fkooman\Rest;
+
+use fkooman\Http\Request;
+use fkooman\Rest\Plugin\Basic\BasicAuthentication;
+use PHPUnit_Framework_TestCase;
+
+class BasicAuthenticationTest extends PHPUnit_Framework_TestCase
+{
+    public function testBasicAuthCorrect()
+    {
+        $request = new Request('http://www.example.org/foo', "GET");
+        $request->setBasicAuthUser('user');
+        $request->setBasicAuthPass('pass');
+
+        $basicAuth = new BasicAuthentication('user', password_hash('pass', PASSWORD_DEFAULT), 'realm');
+        $userInfo = $basicAuth->execute($request);
+        $this->assertEquals('user', $userInfo->getUserId());
+    }
+
+    /**
+     * @expectedException fkooman\Http\Exception\UnauthorizedException
+     * @expectedExceptionMessage invalid credentials
+     */
+    public function testBasicAuthWrongUser()
+    {
+        $request = new Request('http://www.example.org/foo', "GET");
+        $request->setBasicAuthUser('wronguser');
+        $request->setBasicAuthPass('pass');
+
+        $basicAuth = new BasicAuthentication('user', password_hash('pass', PASSWORD_DEFAULT), 'realm');
+        $basicAuth->execute($request);
+    }
+
+    /**
+     * @expectedException fkooman\Http\Exception\UnauthorizedException
+     * @expectedExceptionMessage invalid credentials
+     */
+    public function testBasicAuthWrongPass()
+    {
+        $request = new Request('http://www.example.org/foo', "GET");
+        $request->setBasicAuthUser('user');
+        $request->setBasicAuthPass('wrongpass');
+
+        $basicAuth = new BasicAuthentication('user', 'pass', 'realm');
+        $basicAuth->execute($request);
+    }
+}
